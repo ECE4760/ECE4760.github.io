d5ab1cf8945900cbc4931511e822fa71b414941a
Hunter Adams <hunteradams@dhcp-vl2042-21770.redrover.cornell.edu>
2022-12-14 12:28:49 -0500

    more projects

---
+++ Projects/Fall2022/tc575_rm722_raf269/README.md
@@ -0,0 +1,4 @@
+# tc575_rm722_raf269
+Webpage for ECE 4760 Final Project
+
+https://pages.github.coecis.cornell.edu/tc575/tc575_rm722_raf269/
---
+++ Projects/Fall2022/tc575_rm722_raf269/js/demo/chart-pie-demo.js
@@ -0,0 +1,35 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Direct Digital Synthesis", "Sound Design", "User Input", "Display", "Testing"],
+    datasets: [{
+      data: [20, 30, 20, 10, 20],
+      backgroundColor: ['#4e73df', '#e74a3b', '#36b9cc', '#f6c23e', '#1cc88a'],
+      hoverBackgroundColor: ['#2e59d9', '#e32d1c', '#2c9faf', '#f4b20b', '#17a673'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+    },
+    legend: {
+      display: false
+    },
+    cutoutPercentage: 80,
+  },
+});
